4642ec0ce8af8225ae444367d4803139b53da665
Khairul E. Fahim <64836230+fahim92@users.noreply.github.com>
2020-05-10 03:55:18 +0600

    Add files via upload

---
+++ age.py
@@ -0,0 +1,5 @@
+#This program takes age from the user in years and then converts it to seconds
+x=input('Input your age here')
+x1=float(x)
+x2=x1*12*30*24*60*60
+print('Your age is',x2,'in seconds')
